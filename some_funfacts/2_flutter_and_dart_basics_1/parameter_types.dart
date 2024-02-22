1cfc7b1afd3e28b313c1475460413403372312ce
Estifanos Taye <estifanostaye11@gmail.com>
2024-02-22 21:53:00 +0300

    required

--- some_funfacts/2_flutter_and_dart_basics_1/parameter_types.dart
+++ some_funfacts/2_flutter_and_dart_basics_1/parameter_types.dart
@@ -10,3 +10,8 @@ void demo() {
   add_by_position(2, 3);
   add_by_name(num1: 2, num2: 3);
 }
+
+// named arguments are optional but this makes them mandatory
+int add_by_name_required({required int num1, required int num2}) {
+  return num1 + num2;
+}
