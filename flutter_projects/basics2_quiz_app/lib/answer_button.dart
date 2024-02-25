b59b7a6a375a5b26caccb725258e68026ef0c210
Estifanos Taye <estifanostaye11@gmail.com>
2024-02-25 14:30:26 +0300

    reusable button style

---
+++ flutter_projects/basics2_quiz_app/lib/answer_button.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton(this.text, this.onTap, {super.key});
+
+  final String text;
+  final void Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: ElevatedButton(
+        onPressed: onTap,
+        style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.symmetric(
+              vertical: 10,
+              horizontal: 40,
+            ),
+            backgroundColor: const Color.fromARGB(255, 31, 3, 98),
+            foregroundColor: Colors.white,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(40))),
+        child: Text(text),
+      ),
+    );
+  }
+}
--- flutter_projects/basics2_quiz_app/lib/questions_screen.dart
+++ flutter_projects/basics2_quiz_app/lib/questions_screen.dart
@@ -1,4 +1,6 @@
+import 'package:basics2_quiz_app/answer_button.dart';
 import 'package:flutter/material.dart';
+import 'package:basics2_quiz_app/data/questions.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -10,6 +12,23 @@ class QuestionScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text('');
+    return SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Question 1',
+              style: TextStyle(color: Colors.white),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            AnswerButton("Answer 1", () {}),
+            AnswerButton("Answer 2", () {}),
+            AnswerButton("Answer 3", () {}),
+            AnswerButton("Answer 4", () {}),
+          ],
+        ));
   }
 }
